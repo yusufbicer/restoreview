15a175d0bf20a691455cc56822550989902af72a
Carlos Grijalva <grijalva.carlos@gmail.com>
2017-10-20 16:24:46 -0400

    Add edit form and update controller

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -19,7 +19,24 @@ exports.createStore = async (req, res) => {
 exports.getStores = async (req, res) => {
   // 1. Query the databse for a list of all stores
   const stores = await Store.find();
-  console.log(stores);
+  res.render("stores", { title: "Stores", stores });
+};
 
-  res.render('stores', { title: 'Stores', stores })
-}
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the ID.
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. Configure they are the owner of the store.
+  // 3. Render out the edit form so the user can update it.
+  res.render("editStore", { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // 1. Find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.id}">View Store</a>`)
+  // 2. Redirect them to the store and tell them it worked
+  res.redirect(`/stores/${store._id}/edit`)
+};
--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,8 @@ router.get("/", catchErrors(storeController.getStores));
 router.get("/stores", catchErrors(storeController.getStores));
 router.get("/add", storeController.addStore);
 router.post("/add", catchErrors(storeController.createStore));
+router.get("/stores/:id/edit", catchErrors(storeController.editStore));
+router.post("/add/:id", catchErrors(storeController.updateStore));
+
 
 module.exports = router;
